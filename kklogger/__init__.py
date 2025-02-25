bd0c00f886fdd6401d7d902c8a67fb2e3a418ad1
kazukingh01 <kazukingh01@gmail.com>
2025-02-25 10:41:19 +0900

    modify

--- kklogger/__init__.py
+++ kklogger/__init__.py
@@ -1,2 +1,1 @@
-from kklogger import util
-from kklogger.util.logger import set_logger, set_loglevel, LoggingNameException, MyLogger
+from util.logger import set_logger, set_loglevel, LoggingNameException, MyLogger
